dbf3cdb005ecb93b4a57177e746e9d9d56a6fe9a
Ahmedfelfel <ahmedHfelfel@gmail.com>
2025-10-07 22:29:12 +0300

    .

--- target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-insert into student (id, name, gpa) values (1, 'Alice', 2.5);
-insert into student (id, name, gpa) values (2, 'Bob', 3.2);
-insert into student (id, name, gpa) values (3, 'Charlie', 2.8);
--- target/classes/schema.sql
+++
@@ -1,5 +0,0 @@
-create table student (
-    id INT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    gpa DECIMAL NOT NULL
-);
