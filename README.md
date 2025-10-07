e302d4663838f0840c829c86e12de96311dfa8be
Ahmedfelfel <ahmedHfelfel@gmail.com>
2025-10-07 22:02:19 +0300

    simple crud in springboot

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # java-projects
 <h3>1-simple java applet real time app</h3>
+<h3>2-simple java calender app</h3>
+<h3>3-simple java springboot crud app</h3>
---
+++ target/classes/data.sql
@@ -0,0 +1,3 @@
+insert into student (id, name, gpa) values (1, 'Alice', 2.5);
+insert into student (id, name, gpa) values (2, 'Bob', 3.2);
+insert into student (id, name, gpa) values (3, 'Charlie', 2.8);
